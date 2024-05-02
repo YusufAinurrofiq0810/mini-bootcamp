706e4629890d770db865ae23d4d8ede8b1f082f3
YusufAinurrofiq0810 <120088479+YusufAinurrofiq0810@users.noreply.github.com>
2024-05-02 16:10:31 +0700

    card component

---
+++ src/components/card/card.vue
@@ -0,0 +1,23 @@
+<template>
+      <div class="w-64 h-auto p-4 bg-pink-400 my-1 mx-1 rounded-lg ">
+        <div class="">
+          <p class="text-gray-700">Current Balance</p>
+          <p class="text-gray-700 font-bold text-xl">$5,750.20</p>
+        </div>
+        <div class="flex justify-between mt-2">
+          <p class="text-gray-700">5282 **** **** 1289</p>
+          <p class="text-gray-700">09/25</p>
+        </div>
+      </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CreditCard',
+  };
+  </script>
+  
+  <style scoped>
+  /* Add custom styles here if needed */
+  </style>
+  
---
+++ src/pages/card/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import card from '../../components/card/card.vue';
+</script>
+
+<template>
+    <card>
+        
+    </card>
+</template>
